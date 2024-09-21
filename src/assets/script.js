9cfcc98987ca9b401f4a4c7877fa2728aedd7300
周润发 <shaoyouvip@gmail.com>
2024-09-22 04:44:47 +0800

    Add files via upload

--- src/assets/script.js
+++ src/assets/script.js
@@ -10,10 +10,10 @@ export const state = {
     shadowBlur: 120,
     shadowOffsetX: 1,
     shadowOffsetY: 1,
-    watermark: "@梦爱吃鱼",
+    watermark: "@周润发",
     textSize: 200,
     squareSize: 300,
-    text: '梦爱吃鱼',
+    text: '周润发',
     bgBlur: 3,
     iconBackground: false,
     iconBorder: false
